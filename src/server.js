fbbe071e2266202f2ced48547e77ddf3c8a63aa9
Germano Ciarlini <germano@ar.quei.ro>
2021-05-14 18:02:14 -0300

    Add routes, index and database config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const port = 3001;
 const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Running server on port ${port}`);
