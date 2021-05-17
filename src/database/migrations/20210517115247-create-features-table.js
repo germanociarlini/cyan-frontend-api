05b622396813698b53789a7d366d2c66f3b97161
Germano Ciarlini <germano@ar.quei.ro>
2021-05-17 08:56:32 -0300

    Disallow null values for feature color

---
+++ src/database/migrations/20210517115247-create-features-table.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('features', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      collection_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'collections',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelte: 'CASCADE'
+      },
+      color: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      geometry: {
+        type: Sequelize.GEOMETRY,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull:false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('features');
+  }
+};
