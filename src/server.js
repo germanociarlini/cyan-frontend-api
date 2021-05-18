cc8c3e8e1ab29e3684c641f5bd615424656da73c
Germano Ciarlini <germano@ar.quei.ro>
2021-05-18 17:13:24 -0300

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database');
 
@@ -7,6 +8,7 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
